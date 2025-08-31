c57e3d73ef87368699bed71b4f55ad4296e654eb
root <diwakar.singh@sharajman.com>
2025-08-31 18:08:29 +0530

    strings

--- strings/modify.py
+++ strings/modify.py
@@ -12,7 +12,7 @@ d = "Diwakar Singh Prajapati"
 print(d.replace("D","J"))   # Replace any character
 
 a = "Hello! World"
-print(a.split("!"))
+print(a.split("!")) # Convert the string in to the list with the help of given separator in the string
 
 
 f = "apple, banana, orange, pineapple"
