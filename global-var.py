20ace03dd1513b0b16f3f80d39dc5439ec0f46f8
root <diwakar.singh@sharajman.com>
2025-08-31 11:32:22 +0530

    global-var

--- global-var.py
+++ global-var.py
@@ -1,4 +1,4 @@
-x = "awesome"
+x = "awesome" #global variable
 
 def myfunc():
   print("Python is " + x)
@@ -7,11 +7,25 @@ myfunc()
 
 
 x = "awesome"
-
 def myfunc():
+  x = "fantastic" # local variable
+  print("Python is " + x)
+myfunc()
+print("Python is " + x)
+
+
+def func():
+  global x
+  x = "global inside local"
+func()
+print(x)
+
+
+
+
+x = "awesome"
+def myfunc():
+  global x
   x = "fantastic"
-  print("Python is " + x)
-
 myfunc()
-
 print("Python is " + x)
