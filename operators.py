dc8920f225bfd8fd1e2d3ada908c9b512eab725a
Diwakar Singh <diwakar.sharajman.com>
2025-09-08 09:11:50 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,23 @@
+''' Airthmetic Operators
+    Assignment Operators
+    Comparision Operators
+    Logical Operators
+    Identity Operators
+    Membership Operator
+    Bitwise Operator'''
+
+# Airthmetic Operator
+
+print(10 + 5)   # Addition
+print(10-5)     # Subtraction
+print(10*5)     # Multiplication
+print(int(10/5))    # Division
+print(10%5)     # Modulus
+print(10**5)    # Exponential
+print(10//5)    # Floor division
+
+# Assignment Operator
+
+x = 5
+x &= 3
+print(x)
