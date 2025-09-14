e01b4a725005c68675744873b4d8a8a606a6de43
Diwakar Singh <diwakarsi111@gmail.com>
2025-09-14 16:57:04 +0530

    tuples

---
+++ tuples/tuples.py
@@ -0,0 +1,33 @@
+mytuple = ("apple" , "banana" , "cherry")
+print(mytuple)
+print(type(mytuple))
+
+
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+
+tuple_len = ("apple", "banana", "cherry", "apple", "cherry")
+print(len(tuple_len))
+
+print(tuple_len[1])
+
+
+
+thistuple = ("apple",)
+print(type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
+
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+
+
+tuple1 = ("abc", 34, True, 40, "male")
+
+a = tuple(("apple", "banana", "cherry"))
+print(a)
+print(type(a))
